ba36e71689a22c0053ea94ae15d325495888cca6
Daler-web-dev <77038375+Daler-web-dev@users.noreply.github.com>
2021-09-18 15:18:37 +0500

    sohib molcji

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './App.css'
-import Button from '@material-ui/core/Button'
-import SaveIcon from '@material-ui/icons/Save'
 import Header from './Components/Header/Header'
 import Main from './Components/main/Main'
 
